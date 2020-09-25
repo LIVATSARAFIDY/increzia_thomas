12cf94db4ce7534761c7f215ee0b470b1cdc72ab
LIVATSARAFIDY <57402127+LIVATSARAFIDY@users.noreply.github.com>
2020-09-25 11:04:53 +0300

    exo 1 matrice

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-const text: string = 'coo!!!'
-console.log(text);
+//const text: string = 'coo!!!'
+//console.log(text);
+var i;
+var matrice;
+for (i = 0; i <= 2; i++) {
+    if (i === 0) matrice = matrice + '((0,1,0),';
+    else if (i === 1) matrice = matrice + '(1,1,0),';
+    else matrice = matrice + '(0,0,1))';
+}
+console.log(matrice);
