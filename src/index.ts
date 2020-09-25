50225d2f59262565a72de2b43ebe3ee026d92719
LIVATSARAFIDY <57402127+LIVATSARAFIDY@users.noreply.github.com>
2020-09-25 12:08:08 +0300

    final commit

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,29 @@ for (i = 0; i <= 2; i++) {
     else matrice = matrice + '(0,0,1))';
 }
 console.log(matrice);
+// function somme des entier positive d'un tableau
+function sommeEntier(i, tab) {
+    var longeurTab = tab.length;
+    var j;
+    var somme = 0;
+    if (tab[i] !== 'undifend') {
+        for (j = i; j < longeurTab; j++) {
+            if (tab[j] > 0) somme = somme + tab[j];
+        }
+        console.log(somme);
+    } else console.log("index n'exist pas");
+}
+sommeEntier(0, [0, 3, -5, 12, -18, -20]);
+
+//function arranger tableau
+
+function arrangetab(tab) {
+  var longeurTab = tab.length;
+  var newtab = [];
+  var i;
+  for (i = longeurTab-1; i >= 0 ;i--) {
+      newtab.push(tab[i]);
+  }
+  console.log(newtab);
+}
+arrangetab([0,3,-5,12,-18,-20]);
